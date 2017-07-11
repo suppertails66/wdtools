a3690f15de9fa24c68f4813f2f327f12adda031b
Supper <suppertails66@gmail.com>
2017-07-11 11:30:10 -0500

    Various mgl_str_insr improvements

--- src/mgl_str_insr.cpp
+++ src/mgl_str_insr.cpp
@@ -17,22 +17,15 @@ const static char kanjiFileName[] = "KANJI.FNT";
 
 int main(int argc, char* argv[]) {
   if (argc < 4) {
-    cout << "Usage:" << argv[0] << " transfile srcpath dstpath" << endl;
+    cout << "Usage:" << argv[0] << " srcpath dstpath [transfiles]" << endl;
     return 0;
   }
   
-  string transFileName = argv[1];
-  string srcPrefix = argv[2];
-  string dstPrefix = argv[3];
-  
-  // Read translation file
-  ifstream ifs(transFileName.c_str());
-  TranslationFile translationFile;
-  translationFile.load(ifs);
-  ifs.close();
-  
+  string srcPrefix = argv[1];
+  string dstPrefix = argv[2];
+    
   // Read KANJI.FNT
-  ifs.open((srcPrefix + kanjiFileName).c_str());
+  ifstream ifs((srcPrefix + kanjiFileName).c_str());
   int kanjiFileSize = fsize(ifs);
   char* kanjiBuffer = new char[kanjiFileSize];
   ifs.read(kanjiBuffer, kanjiFileSize);
@@ -40,59 +33,70 @@ int main(int argc, char* argv[]) {
   
   int kanjiPutPos = kanjiFreeSpaceStart;
   
-  // Handle each file
-  for (FilenameTranslationEntryMap::iterator it
-          = translationFile.filenameEntriesMap.begin();
-       it != translationFile.filenameEntriesMap.end();
-       ++it) {
-    ifstream ifs((srcPrefix + it->first).c_str());
-    int fileSize = fsize(ifs);
-    char* fileBuffer = new char[fileSize];
-    ifs.read(fileBuffer, fileSize);
+  // Read all input translation files in sequence
+  for (unsigned int i = 3; i < argc; i++) {
+    string transFileName = argv[i];
+    
+    // Read translation file
+    ifstream ifs(transFileName.c_str());
+    TranslationFile translationFile;
+    translationFile.load(ifs);
     ifs.close();
     
-    TranslationEntryCollection& entries = it->second;
-    for (unsigned int i = 0; i < entries.size(); i++) {
-      TranslationEntry& entry = entries[i];
+    // Handle each file
+    for (FilenameTranslationEntryMap::iterator it
+            = translationFile.filenameEntriesMap.begin();
+         it != translationFile.filenameEntriesMap.end();
+         ++it) {
+      ifstream ifs((srcPrefix + it->first).c_str());
+      int fileSize = fsize(ifs);
+      char* fileBuffer = new char[fileSize];
+      ifs.read(fileBuffer, fileSize);
+      ifs.close();
       
-      // place at original location if possible
-//      cerr << entry.originalSize << endl;
-      if (entry.translatedText.size() <= entry.originalSize) {
-        strcpy(fileBuffer + entry.sourceFileOffset,
-               entry.translatedText.c_str());
-      }
-      // otherwise, move to KANJI.FNT
-      else {
-        int startPos = kanjiPutPos;
-        kanjiPutPos += entry.translatedText.size() + 1;
+      TranslationEntryCollection& entries = it->second;
+      for (unsigned int i = 0; i < entries.size(); i++) {
+        TranslationEntry& entry = entries[i];
         
-        if (kanjiPutPos > kanjiFreeSpaceEnd) {
-          cerr << "Error: Not enough space in KANJI.FNT" << endl;
-          return 1;
+        // place at original location if possible
+  //      cerr << entry.originalSize << endl;
+        if (entry.translatedText.size() <= entry.originalSize) {
+          strcpy(fileBuffer + entry.sourceFileOffset,
+                 entry.translatedText.c_str());
         }
-        
-        cout << "Moved to KANJI.FNT, " << hex << startPos
-          << ": " << entry.sourceFile << ", " << entry.sourceFileOffset << ", "
-            << entry.translatedText << endl;
-        
-        strcpy(kanjiBuffer + startPos,
-               entry.translatedText.c_str());
-        
-        // update pointers
-        unsigned int newPointer = kanjiLoadAddress + startPos;
-        
-        for (unsigned int i = 0; i < entry.pointers.size(); i++) {
-          char* dst = fileBuffer + entry.pointers[i];
-          ByteConversion::toBytes(newPointer, dst, 4,
-            EndiannessTypes::big, SignednessTypes::nosign);
+        // otherwise, move to KANJI.FNT
+        else {
+          int startPos = kanjiPutPos;
+          kanjiPutPos += entry.translatedText.size() + 1;
+          
+          if (kanjiPutPos > kanjiFreeSpaceEnd) {
+            cerr << "Error: Not enough space in KANJI.FNT" << endl;
+            return 1;
+          }
+          
+          cout << "Moved to KANJI.FNT, " << hex << startPos
+            << ": " << entry.sourceFile << ", " << entry.sourceFileOffset << ", "
+              << entry.translatedText << endl;
+          
+          strcpy(kanjiBuffer + startPos,
+                 entry.translatedText.c_str());
+          
+          // update pointers
+          unsigned int newPointer = kanjiLoadAddress + startPos;
+          
+          for (unsigned int i = 0; i < entry.pointers.size(); i++) {
+            char* dst = fileBuffer + entry.pointers[i];
+            ByteConversion::toBytes(newPointer, dst, 4,
+              EndiannessTypes::big, SignednessTypes::nosign);
+          }
         }
       }
+      
+      ofstream ofs((dstPrefix + it->first).c_str(), ios_base::trunc);
+      ofs.write(fileBuffer, fileSize);
+      
+      delete fileBuffer;
     }
-    
-    ofstream ofs((dstPrefix + it->first).c_str(), ios_base::trunc);
-    ofs.write(fileBuffer, fileSize);
-    
-    delete fileBuffer;
   }
   
   // write KANJI.FNT
