6c656b975de018569ebf717d19613c001b69d18e
Misty De Meo <mistydemeo@gmail.com>
2017-08-12 15:46:49 -0700

    l2eb_data: make UPD optional
    The Saturn version uses the same packed data format as the PlayStation version,
    with file `2` serving the role of `DATA.PAK` and `1` serving the role of
    `DATA.IDX`. However, the Saturn has no filename mapping table; the files are
    all unnamed.
    
    This commit updates l2eb_data to allow unpacking a file without an `.UPD` file;
    files will just be given numeric filenames in this case.
    
    In order to accommodate varying numbers of arguments, the .UPD file and the
    filename prefix have been turned into options.

--- src/l2eb_data.cpp
+++ src/l2eb_data.cpp
@@ -54,32 +54,55 @@ void createDirectoryForFile(string name) {
   createDirectory(getDirectory(name));
 }
 
+char* get_opt(char** begin, char** end, const std::string option) {
+  char** idx = std::find(begin, end, option);
+  if (idx != end && ++idx != end) {
+    return *idx;
+  } else {
+    return 0;
+  }
+}
+
 int main(int argc, char* argv[]) {
+  bool upd_available = false;
+  ifstream upd;
+  char* upd_name;
+  char* prefix_c;
+
   if (argc < 2) {
     printf("No IDX specified!\n");
     return 1;
   }
   if (argc < 3) {
-    printf("No UPD specified!\n");
-    return 1;
-  }
-  if (argc < 4) {
     printf("No PAK specified!\n");
     return 1;
   }
+  upd_name = get_opt(argv, argv + argc, "--upd");
+  if (upd_name == 0) {
+    printf("No UPD specified! Files will be extracted with generic names.\n");
+  } else {
+    upd_available = true;
+    upd.open(upd_name, ios_base::binary);
+  }
   
   ifstream idx(argv[1], ios_base::binary);
-  ifstream upd(argv[2], ios_base::binary);
-  ifstream pak(argv[3], ios_base::binary);
+  ifstream pak(argv[2], ios_base::binary);
   
   string prefix;
-  if (argc >= 5) prefix = string(argv[4]);
+  prefix_c = get_opt(argv, argv + argc, "--prefix");
+  if (prefix_c != 0) {
+    prefix = string(prefix_c);
+  }
   
   // get filename table pointer and seek to it
-  upd.seekg(0x10);
-  upd.seekg(TSerialize::readInt(
-    upd, 4, EndiannessTypes::little, SignednessTypes::nosign));
+  if (upd_available) {
+    upd.seekg(0x10);
+    upd.seekg(TSerialize::readInt(
+      upd, 4, EndiannessTypes::little, SignednessTypes::nosign));
+  }
   
+  int fileno = 0;
+
   // read entries until all data has been read
   while (true) {
     char next = idx.get();
@@ -92,10 +115,14 @@ int main(int argc, char* argv[]) {
       idx, 2, EndiannessTypes::big, SignednessTypes::nosign);
     
     string filename = prefix;
-    do {
-      next = upd.get();
-      filename += next;
-    } while (next != 0);
+    if (upd_available) {
+      do {
+        next = upd.get();
+        filename += next;
+      } while (next != 0);
+    } else {
+      filename += std::to_string(fileno);
+    }
     
     // replace backslashes
     for (int i = 0; i < filename.size(); i++) {
@@ -114,6 +141,8 @@ int main(int argc, char* argv[]) {
     ofs.write(buffer, realSize);
     
     delete buffer;
+
+    fileno += 1;
   }
   
   return 0;
